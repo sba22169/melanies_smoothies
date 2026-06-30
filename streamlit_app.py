3bab1acc093e36f09181935a87436b4130612984
Ambrose Desmond <119744823+sba22169@users.noreply.github.com>
2026-06-30 14:30:49 +0100

    Create streamlit_app.py
    copied from Custom Smoothies SiS app

---
+++ streamlit_app.py
@@ -0,0 +1,55 @@
+# Import python packages
+import streamlit as st
+from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col , when_matched
+
+# Write directly to the app
+st.title(f":cup_with_straw: Customize Your Smoothie :cup_with_straw: ")
+st.write(
+  """Choose the fruits you want in your custom smoothie!
+  """
+)
+
+
+name_on_order = st.text_input('Name on Smoothie:')
+st.write('The name on the smoothie is :',name_on_order )
+
+#option = st.selectbox(
+    #'What is your favourite fruit?',
+    #('Banana','Strawberries','Peaches'))
+#st.write('You selected:',option)
+
+
+### Display the Fruit Options List in Your Streamlit in Snowflake (SiS) App
+
+session = get_active_session()
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+# st.dataframe(data=my_dataframe, use_container_width=True)
+
+ingredients_list = st.multiselect(
+    'Choose your fruit (5 max):', my_dataframe , max_selections=5)
+
+if ingredients_list:
+    #st.write(ingredients_list)
+    #st.text(ingredients_list)
+    ingredients_string = ''
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ' '
+    #st.write(ingredients_string)
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+        values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
+    #st.write(my_insert_stmt)
+    #st.stop()
+    if ingredients_string:
+        session.sql(my_insert_stmt).collect()
+        st.success('Your Smoothie is ordered!', icon="✅")
+
+#og_dataset = session.table("smoothies.public.orders")
+#edited_dataset = session.create_dataframe(editable_df)
+#og_dataset.merge(edited_dataset, (og_dataset['ORDER_UID'] == edited_dataset['ORDER_UID'])
+#, [when_matched().update({'ORDER_FILLED': edited_dataset['ORDER_FILLED']})]
+                #)
+
+
+    
+    
