c8c1c4b2f31239c5d338ab79e7ec2f34fff4f8ac
Ambrose Desmond <119744823+sba22169@users.noreply.github.com>
2026-07-01 09:14:58 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import streamlit as st
 # from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col , when_matched
 
-new section to display smoothiefroot nutrition information#
+# new section to display smoothiefroot nutrition information#
 
 import requests  
 # smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
