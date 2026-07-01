a46d92e5720e494d024e6dc97d281bf87b5c89db
Ambrose Desmond <119744823+sba22169@users.noreply.github.com>
2026-07-01 22:08:43 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,7 @@ session = cnx.session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 st.dataframe(data=my_dataframe, use_container_width=True)
-ST.stop()
+st.stop()
 
 ingredients_list = st.multiselect(
     'Choose your fruit (5 max):', my_dataframe , max_selections=5)
